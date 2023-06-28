8015922366230cb5e10cdb24482d757cf4626c8a
rootexploitsPmo <57397830+rootexploitsPmo@users.noreply.github.com>
2023-06-28 12:27:54 +0200

    Modificaciones en el passive Renfe
    Adaptamos las expresiones regulares para el nuevo formato del pdf que baja de renfe.

--- src/components/renfe/BodyRenfe.vue
+++ src/components/renfe/BodyRenfe.vue
@@ -39,21 +39,22 @@ components: {
     GetData(data){
      // console.log(data); contiene el texto para analisar
     var valor_Textoarea = data
-    
 
 // INICIO DE MIGRASION 
 
 //ESTE CONTENIDO CONTIENE LA SEGUNDA PARTE DE LOS RM LOS QUE USUAMOS EN  EL COMPONENTE PackRMS 
     var numero_veces=valor_Textoarea.match(/(?<=Billete\/Ticket:\S{0,}\s{0,})\d{0,}|(?<=Nº\s{0,}Billete:\s{0,})\w{1,}(?=\s|\n)/g); // CONTIENE EL NUMERO DE BILLETES ENCONTRADOS
     
-    var rm_billete_tarifa=valor_Textoarea.match(/(?<=TARIFA.*\-\s)\d{0,6}\s\d{0,}\-.*\n.*/g); //CONTIENE EL CONTENIDO DE LOS BILLETES ENCONTRADOS
+    
+    var rm_billete_tarifa=valor_Textoarea.match(/\w{1,}.*(?=\sT\.C|\sMetalico)/g); //CONTIENE EL CONTENIDO DE LOS BILLETES ENCONTRADOS
     //limpiamos el array de RM
     var rm_lista_tarifa=rm_billete_tarifa.map((x)=>{ // CON ESTE MAP CONSEGUIMOS SUSTITUIR O QUITAR EL SIGNO DEL EURO YA QUE NO LO NECESITAMOS
-      var nuevo_valor=x.replace("€","")
-      nuevo_valor="RM*RENFE-"+nuevo_valor
-      return nuevo_valor
-    })
 
+   
+      let   nuevo_valor="RM*RENFE-"+x
+      
+        return nuevo_valor
+    })
 
 
 //ESTE CONTENIDO CONTIENE LA SEGUNDA PARTE DE LOS RM 
@@ -67,32 +68,47 @@ var array_rm_LC=[]
 for(var i=1; i<=numero_veces.length; i++){
       var fechas=valor_Textoarea.match(/\d{2}\/\w{3}\.\/\d{4}|\d{2}\/\d{2}\/\d{4}\n/g);
       var fecsal=KitPassive.formatoFecha(fechas[contadorCero]);
-      var CODLC=KitPassive.buscar_Match(/(?<=TREN:\D{0,}\s{0,})\w{1,}|(?<=Localizador:\s{0,})\w{1,}(?=\s)/,valor_Textoarea);
+      var codLc=KitPassive.buscar_Match(/(?<=TREN:\D{0,}\s{0,})\w{1,}|(?<=Localizador:\s{0,})\w{1,}(?=\s)/,valor_Textoarea);
       
-
       var ciudad_Origen=valor_Textoarea.match(/(?<=Origen:\s{0,}\s{0}).*/g);
       var ciudad_Destino=valor_Textoarea.match(/(?<=Destino:\s{0,}\s{0}).*/g);
       var horas=valor_Textoarea.match(/\d{2}\:\d{2}(?=\n)/g);
-      var numeros_de_tren=valor_Textoarea.match(/(?<=TARIFA\s.*\-\s)\d{1,}/g);
+     
+      var numeros_de_tren=valor_Textoarea.match(/\d{4,}(?=\n\d{1,}\/\d{1,}\/\d{1,}\n\d{1,}\/\d{1,}\/\d{1,})/g);
       var transporte=valor_Textoarea.match(/(?<=Destino:.*\n)\w{1,}(?=\s\w{1,}.*\n)/g);  
       var coches=valor_Textoarea.match(/(?<=Coche:\s{0,})\d{1,}/g);
       var texto_oches=/\d{2}/.exec(coches[contadorCero_tk]);
+
       if(texto_oches===null){
         coches[contadorCero_tk]="000"+coches[contadorCero_tk];
       }else{
         coches[contadorCero_tk]="00"+coches[contadorCero_tk];
       }
+      debugger
       var plaza=valor_Textoarea.match(/(?<=Plaza:\s{0,})\d{1,}\w{1,}/g);
       var texto_plaza=/\d{2}/.exec(plaza[contadorCero_tk]);
-      var total_totales=valor_Textoarea.match(/(?<=TOTAL\s{0,})(\d{1,}\,\d{0,}|\d{1,}\.\d{1,}\,\d{0,})/g);
-      if(texto_plaza===null){
-        plaza[contadorCero_tk]="0"+plaza[contadorCero_tk];
-      }
-      var clases=valor_Textoarea.match(/(?<=Destino:\s{0,}\s{0}.*\n\w{1,}\s)\w{1,}/g);
-      var tar=valor_Textoarea.match(/(?<=TARIFA\s).*(?=\s\-)/g);
       
-      var primersegmento="RU1AHK1XZJ "+fecsal +"*RENFE-LC:"+CODLC+" TK:"+numero_veces[contadorCero_tk]+ ", ORIG:"+ciudad_Origen[contadorCero_tk]+" "+horas[contadorCero]+", DEST:"+ciudad_Destino[contadorCero_tk]+" "+horas[conta_destino]+", TREN:"+numeros_de_tren[contadorCero_tk]+" "+transporte[contadorCero_tk]+" COCHE:"+coches[contadorCero_tk]+" PLZ:"+plaza[contadorCero_tk]+" C:"+clases[contadorCero_tk]+" TAR:"+tar[contadorCero_tk];
+      var total_totales=valor_Textoarea.match(/(?<=TOTAL\s{0,})(\d{1,}\,\d{0,}|\d{1,}\.\d{1,}\,\d{0,})/g);
+      
+      if(texto_plaza===null){
+        
+        plaza[contadorCero_tk]="0"+plaza[contadorCero_tk];
+      
+
+      }
+      
+      var clases=valor_Textoarea.match(/(?<=Destino:\s{0,}\s{0}.*\n\w{1,}\s)(ESTÁNDAR|CONFORT)/g);
+      var tar=valor_Textoarea.match(/(.*)(?=\sT\.C|\sMetalico)/g);
+      console.log(numeros_de_tren)
+      
+      //s1t
+      console.log("PLZ:"+plaza[contadorCero_tk]+" C:"+clases[contadorCero_tk]+" TAR:"+tar[contadorCero_tk])
+      console.log(" C:"+clases[contadorCero_tk])  
+      console.log("TAR:"+tar[contadorCero_tk])
+
+      var primersegmento="RU1AHK1XZJ "+fecsal +"*RENFE-LC:"+codLc+" TK:"+numero_veces[contadorCero_tk]+ ", ORIG:"+ciudad_Origen[contadorCero_tk]+" "+horas[contadorCero]+", DEST:"+ciudad_Destino[contadorCero_tk]+" "+horas[conta_destino]+", TREN:"+numeros_de_tren[contadorCero_tk]+" "+transporte[contadorCero_tk]+",COCHE:"+coches[contadorCero_tk]+" PLZ:"+plaza[contadorCero_tk]+",C:"+clases[contadorCero_tk]+" TAR:"+tar[contadorCero_tk];
      array_rm_P.push(primersegmento);
+    
 
 //SEGUNDA PARTE 
  var forma_de_pago=/\d{1,}\*{1,}\d{1,}/.test(valor_Textoarea);
@@ -102,7 +118,7 @@ for(var i=1; i<=numero_veces.length; i++){
         }else{
           fp="CASH";
         }
-       var lineastk="RM***RENFE LC:"+CODLC+",TK:"+numero_veces[contadorCero_tk]+",IMP:"+total_totales[contadorCero_tk]+",TASA:"+"0.00"+",GESTION:"+"0.00"+" FP:"+fp;
+       var lineastk="RM***RENFE LC:"+codLc+",TK:"+numero_veces[contadorCero_tk]+",IMP:"+total_totales[contadorCero_tk]+",TASA:"+"0.00"+",GESTION:"+"0.00"+" FP:"+fp;
        array_rm_LC.push(lineastk)
        array_rm_LC.push(rm_lista_tarifa[contadorCero_tk])
       if(array_rm_LC.length===4){
@@ -116,16 +132,16 @@ for(var i=1; i<=numero_veces.length; i++){
       contadorCero=contadorCero+2;
    
 }
+
 if(array_rm_LC.length===2){
   this.array_renfe.push([array_rm_LC])
-
 }
 
-      var ultima_1="RM *ACC"+CODLC;
-       var ultima_2="RM*ACERNF-"+CODLC;
+      var ultima_1="RM *ACC"+codLc;
+       var ultima_2="RM*ACERNF-"+codLc;
        var ultima_3="RM*PNR HAS PASSED ROBOTICS";
        var TC=/((?<=T\.C\.\:\s{0,})\S.*)|(?<=TARIFA.*)Metálico/.exec(valor_Textoarea)
-       if(TC[0]==="Metálico"){
+       if(TC[0]==="Metalico"){
         TC[0]="CASH"
        }
        var ultima_4="RM*RENFE FP:"+TC[0]
